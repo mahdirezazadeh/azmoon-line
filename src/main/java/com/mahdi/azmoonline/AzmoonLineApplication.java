08204d55bb7f0d6a20e684ebdb42b997e0620f2f
Mahdi Rezazadeh <riza.rs2000@gmail.com>
2023-03-19 18:24:21 +0330

    init project

---
+++ src/main/java/com/mahdi/azmoonline/AzmoonLineApplication.java
@@ -0,0 +1,13 @@
+package com.mahdi.azmoonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzmoonLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzmoonLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mahdi/azmoonline/AzmoonLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahdi.azmoonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzmoonLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
