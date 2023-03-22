d99fa55394d9c7e824dfa3e0a6b5a806ab579fca
Mahdi Rezazadeh <riza.rs2000@gmail.com>
2023-03-22 15:16:09 +0330

    Add base package

---
+++ src/main/java/com/mahdi/azmoonline/base/web/rest/BaseRestFul.java
@@ -0,0 +1,104 @@
+package com.mahdi.azmoonline.base.web.rest;
+
+import com.mahdi.azmoonline.base.BaseDTO;
+import com.mahdi.azmoonline.base.BaseEntity;
+import com.mahdi.azmoonline.base.mapper.BaseMapper;
+import com.mahdi.azmoonline.base.service.BaseService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.Serializable;
+import java.util.List;
+import java.util.Optional;
+
+public class BaseRestFul<E extends BaseEntity<PK>, D extends BaseDTO<PK>, PK extends Serializable,
+        S extends BaseService<E, PK>, M extends BaseMapper<E, D, PK>> {
+
+    private final S service;
+
+    private final M mapper;
+
+    public BaseRestFul(S service, M mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<D>> getAll() {
+        return ResponseEntity.ok(
+                mapper.convertListEntityToDTO(
+                        service.findAll()
+                )
+        );
+    }
+
+    @GetMapping("/pageable")  /*  /user/pageable?size=5&page=1&sort=id,desc&sort=age,desc    */
+    public ResponseEntity<Page<D>> getAll(Pageable pageable) {
+
+        Page<E> page = service.findAll(pageable);
+
+        return ResponseEntity.ok(
+                page.map(
+                        mapper::convertEntityToDTO
+                )
+        );
+
+    }
+
+
+    @GetMapping("/{id}") /* /user/5 */
+    public ResponseEntity<D> findById(@PathVariable("id") PK id) {
+        Optional<E> optionalE = service.findById(id);
+
+        return optionalE.map(e -> ResponseEntity.ok(
+                mapper.convertEntityToDTO(
+                        e
+                )
+        )).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @DeleteMapping("/{id}") /* /user/5 */
+    public ResponseEntity<Void> delete(@PathVariable("id") PK id) {
+        service.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<D> save(@RequestBody D d) {
+
+        if (d.getId() != null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        E entity = service.save(
+                mapper.convertDTOToEntity(d)
+        );
+
+        return ResponseEntity.ok(
+                mapper.convertEntityToDTO(entity)
+        );
+
+    }
+
+    @PutMapping
+    public ResponseEntity<D> update(@RequestBody D d) {
+
+        if (d.getId() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        E entity = service.save(
+                mapper.convertDTOToEntity(d)
+        );
+
+        return ResponseEntity.ok(
+                mapper.convertEntityToDTO(entity)
+        );
+
+    }
+
+
+}
